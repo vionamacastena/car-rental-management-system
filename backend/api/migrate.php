e92da024d5aa4b684a8918d063b0f2d5a15f8f58
vionamacastena <130097510+vionamacastena@users.noreply.github.com>
2026-09-16 20:01:03 +0200

    remove migrate endpoint

--- backend/api/migrate.php
+++
@@ -1,7 +0,0 @@
-<?php
-require __DIR__.'/../vendor/autoload.php';
-$app = require_once __DIR__.'/../bootstrap/app.php';
-$kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
-$kernel->call('migrate', ['--force' => true]);
-$kernel->call('db:seed', ['--force' => true]);
-echo "OK\n";
